193bc640f123a4e453e267b525d883b27eca5ac1
rsudra <rsudra@hotmail.com>
2020-03-02 19:55:49 +0000

    fixed typo

--- infra/create-app-service.ps1
+++ infra/create-app-service.ps1
@@ -7,4 +7,4 @@ New-AzureRmResourceGroup -Name "Soup2NutsRG" -Location "West US"
 
 New-AzureRmAppServicePlan -ResourceGroupName "Soup2NutsRG" -Name "soup2nuts-app-plan" -Location "West US" -Tier "Basic" -NumberofWorkers 2 -WorkerSize "Small"
 
-New-AzureRmWebApp -ResourceGroupName "Soup2NutsRG" -Name "ContosoSite" -Location "West US" -AppServicePlan "soup2nuts-app-plan"
+New-AzureRmWebApp -ResourceGroupName "Soup2NutsRG" -Name "soup2nuts-app" -Location "West US" -AppServicePlan "soup2nuts-app-plan"
