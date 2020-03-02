c5a443d3de56b17d44bfa53d74abb544a153f2e5
rsudra <rsudra@hotmail.com>
2020-03-02 19:50:33 +0000

    init script

--- infra/create-app-service.ps1
+++ infra/create-app-service.ps1
@@ -1,4 +1,8 @@
 
+Install-Module AzureRM 
+
+Import-Module AzureRM
+
 New-AzureRmResourceGroup -Name "Soup2NutsRG" -Location "West US"
 
 New-AzureRmAppServicePlan -ResourceGroupName "Soup2NutsRG" -Name "soup2nuts-app-plan" -Location "West US" -Tier "Basic" -NumberofWorkers 2 -WorkerSize "Small"
