c3a69927078072857df5a84f261583ee3a73e634
rsudra <rsudra@hotmail.com>
2020-03-03 10:56:15 +0000

    add dummy test

---
+++ src/test/java/com/javabycode/springboot/HelloWorldControllerTest.java
@@ -0,0 +1,14 @@
+package com.javabycode.springboot;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class HelloWorldControllerTest {
+
+    @Test
+    public void testStuff() {
+        fail("No tests");
+    }
+
+}
